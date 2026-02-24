ed12e14a5217f80b1d94074911fba294f0173436
Sandaru Samintha <sandarusaminthagunarathna@gmail.com>
2026-02-24 16:51:30 +0530

    create the footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import Header from './components/common/Header';
+import Footer from './components/common/Footer';
+import { Router } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <AuthProvider>
+        <AlertProvider>
+          <div className="App">
+          <Header/>
+          </div>
+        </AlertProvider>
+      </AuthProvider>
+    </Router>
+    
   );
 }
 
